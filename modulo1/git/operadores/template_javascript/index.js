c97afb5f3656d5550de1be20613c8ce37c0515ba
Deodato33 <robsondeodato@yahoo.com.br>
2022-02-15 23:23:15 -0300

    Aula operadores Logicos

---
+++ modulo1/git/operadores/template_javascript/index.js
@@ -0,0 +1,107 @@
+
+/*
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2 
+console.log("a. ", true) // é false por que todos boleanos.
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", false)  // O bool3 é diferente dos demais, afirmação false
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", true) //true por que bool1 e boo12 são diferntes e não false
+
+console.log("d. ", typeof resultado) // boleanos (true e false)
+
+
+2
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) // Será duas strings e não poderá somar irá, concatenar.
+
+3
+
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero // Terei que mudar de strings para numreral.
+
+console.log (soma) 
+*/
+
+// Exercícios de escrita códigos
+
+
+let idadeUsuario = Number(prompt("Qual a sua idade?"))
+let idadeDoMelhorAmigo = Number(prompt("Qual a idade do meu melhor amigo?"))
+
+console.log (idadeUsuario > idadeDoMelhorAmigo)
+console.log (idadeUsuario - idadeDoMelhorAmigo)
+
+
+3
+
+let idadeEmAnos = Number(prompt("Qual sua idade em anos?"))
+console.log (idadeEmAnos)
+
+const idadeEmMeses = 12
+console.log ("A minha idade em meses é " , idadeEmAnos * idadeEmMeses) 
+const idadeEmDias = 30
+console.log ("A minha idade em dias é" , idadeEmAnos * idadeEmDias * idadeEmMeses)
+const idadeEmHoras = 24
+console.log ("A minha idade em horas é" , idadeEmAnos * idadeEmDias * idadeEmHoras)
+
+ 
+let numeroPar = Number (prompt("Digite um numero par"))
+console.log (numeroPar % 2)
+
+//O padrão resultante é zero ao solicitar a sobra da divisão por 2.
+
+//Os numeros impar não são divisiveis, porém sempre existirá antes dele que será divisivel por dois.
+
+
+let digiteNumero1 = Number (prompt("Digite primeiro numero ?"))
+let digiteNumero2 = Number (prompt("Digite o segundo numero"))
+console.log ("O primeiro numero é maior que segundo?", digiteNumero1 > digiteNumero2)
+console.log ("O primeiro numero é igual ao segundo?" , digiteNumero1 === digiteNumero2)
+console.log ("O primeiro numero é divisível pelo segundo?" , digiteNumero1 % digiteNumero2 === 0)
+console.log ("O segundo numero é divisível pelo primeiro?", digiteNumero2 % digiteNumero1 === 0)
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
