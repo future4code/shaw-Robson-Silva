d61923a80086fe8983a95869f6bd0cb2d3687482
Deodato33 <robsondeodato@yahoo.com.br>
2022-02-16 21:45:38 -0300

    index.js

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,78 @@
+
+
+// let array
+// console.log('a. ', array) // Sempre que não for atribuido nenhum valor o resultado será undefined.
+
+// array = null
+// console.log('b. ', array)  // Neste caso atribuiu-se um valor que é Null.
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length) // Contém 11 elementos.
+
+// let i = 0
+// console.log('d. ', array[i]) // O elemento que consta em colchete 3.
+
+// array[i+1] = 19
+// console.log('e. ', array) // São 11 elementos.
+
+// const valor = array[i+6]
+// console.log('f. ', valor) // O elemento no indice 6 é o 9.
+
+
+
+// //Exercico 2
+
+// const frase = prompt("Digite uma frase") 
+
+// //Qual será o valor impresso no console se a entrada do usuário for: `"Subi em um ônibus em Marrocos"`?
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) 
+// // SUBI EM UM ÔNIBUS EM MIRROCOS. 29 CARACTÉRIES.
+
+
+//Exercico de Escrita
+
+//Exercicio 1
+
+// let nome 
+// let email 
+// nome = prompt("Qual e seu nome?")
+// email = prompt ("Qual e seu e-mail?")
+
+// console.log( `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}`)
+   
+// // Exercicio 2
+
+// let comidaFavorita = [ "Arroz", "batatas", "frango", "camarão", "sorvete"]
+// console.log("Essas são minhas comidas favoritas:") 
+// // console.log (comidaFavorita[0])  
+// // console.log (comidaFavorita[1])
+// // console.log (comidaFavorita[2])
+// // console.log (comidaFavorita[3])
+// // console.log (comidaFavorita[4])
+// let qualSuaComida = prompt("Qual sua comida favorita")  
+// comidaFavorita[1] = qualSuaComida
+// console.log (comidaFavorita[0])  
+// console.log (comidaFavorita[1])
+// console.log (comidaFavorita[2])
+// console.log (comidaFavorita[3])
+// console.log (comidaFavorita[4])
+
+//Exercico 3
+ /*
+let listasDeTarefa = [] 
+let homeWork1 = prompt('Fale uma tarefa de que você precisa fazer ao acordar?')
+let homeWork2 = prompt('Fale uma tarefa de que você precisa fazer a tarde?')
+let homeWork3 = prompt('Fale uma tarefa de que você precisa fazer a noite?')
+listasDeTarefa.push(homeWork1)
+listasDeTarefa.push(homeWork2)
+listasDeTarefa.push(homeWork3)
+console.log(listasDeTarefa)
+
+const numeroDoIndice = prompt('Digite o indice de uma tarefa realizada:')
+const tarefaRealizada = listasDeTarefa.splice(numeroDoIndice,1)
+console.log(listasDeTarefa)
+
+*/
+
+
