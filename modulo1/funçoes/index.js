88ab51736ecd2b32a371ec9d52d9d910c53ca2ce
Deodato33 <robsondeodato@yahoo.com.br>
2022-02-17 23:51:45 -0300

    funcoes

---
+++ modulo1/funçoes/index.js
@@ -0,0 +1,130 @@
+//Exercícios de interpretação de código
+
+//01
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+//console.log(minhaFuncao(2)) 
+//console.log(minhaFuncao(10)) 
+
+// O resultado será 10.
+// O resultado será 50.
+
+//02
+
+// let textoDoUsuario = prompt("Insira um texto"); 
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura") 
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+// // A  Resp: Esta função faz com que ....
+
+// // B
+//  `Eu gosto de cenoura` //true
+//  `CENOURA é bom pra vista`//true
+//  `Cenouras crescem na terra` //true
+
+ 
+//Exercícios de escritas e codigos
+
+
+// function dadosPessoais ( ) {
+
+// let nome = "robson"
+// let souIdade = 32
+// let cidadeOndeMora = "Campinas"
+// let profissao = "estudante"
+
+// console.log("Eu sou",nome, "minha idade é",souIdade, "Eu moro na cidade de", cidadeOndeMora, "Minha profissão é", profissao)  
+//  return dadosPessoais 
+// }
+// dadosPessoais ( ) 
+
+// //B
+
+// function dadosPessoais1 (p1, p2, p3, p4) {
+
+// console.log(`Eu "seu nome ${p1}, tenho ${p2} anos, moro em ${p3} e sou ${p4}.`)    
+// }
+// let nome = prompt("Digite seu nome")
+// let idade = prompt ("Me diz sua idade")
+// let endereço = prompt ("Qual seu endereço")
+// let profissao = prompt("Qual a sua profissão")
+
+// dadosPessoais1(nome,idade,endereço,profissao)
+
+// 3
+
+// function soma (v1,v2) {
+// let somaValor = v1+v2 
+// return somaValor 
+// }
+
+// function subtrair (v1,v2) {
+// let somaValor = v1-v2 
+// return somaValor 
+// }
+
+// function dividir (v1,v2) {
+// let somaValor = v1/v2 
+// return somaValor 
+// }
+
+// function multipicar (v1,v2) {
+// let somaValor = v1*v2 
+// return somaValor 
+// }
+
+// let valor1 =  Number (prompt ("Digite um valor"))
+// let valor2 = Number (prompt("Digite um valor"))
+
+
+// console.log (soma(valor1,valor2))
+// console.log (subtrair(valor1,valor2))
+// console.log (dividir(valor1,valor2))
+// console.log (multipicar(valor1,valor2))
+
+//2
+
+//A
+
+// function soma (v3,v6) {
+// let somaValor = v3+v6 
+// return somaValor
+// }
+// console.log (soma(3,6))
+
+// //B
+
+// function  primeiro1 (v1,v2) {
+// let maiorOuIgual = v1 >= v2
+// return maiorOuIgual
+// }
+
+// console.log(primeiro1(3,7))
+
+//C
+
+// function  umNumero (v1) {
+// let parOuimpar = v1 % 2 ==0
+// return parOuimpar
+// }
+
+// console.log(umNumero(7))
+
+//D
+
+// function listastrigs (strings1) {
+// let tamanho1 = strings1.length 
+// let tamanho2 = strings1.toUpperCase()    
+// console.log(`O tamanho da frase é ${tamanho1}, E a frase maiuscula é ${tamanho2}`)
+
+// }
+// listastrigs('Robson')
+
+
