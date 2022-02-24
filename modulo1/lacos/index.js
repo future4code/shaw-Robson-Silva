f271613554cc37149d1c6f8e2b3cea81e0c3f449
Deodato33 <robsondeodato@yahoo.com.br>
2022-02-23 23:45:36 -0300

    lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,70 @@
+//Exercícios de interpretação de código
+
+//1) Vai aparecer 10 
+
+//2) 
+
+//A) Íra ser impresso no console, os números maiores que > 18 sendo, 19, 21, 23, 25, 27, 30.
+
+//B) O codigo está escrito em loop até o valordo indice bater 4. O valor impresso será 10,
+// pois para cada valor soma-se ao indice ao valor anterior, sendo imprsso no console.
+
+//3 Daria erro
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < (quantidadeTotal)) {
+// let linha = ""
+// for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+// linha += "*"
+// }
+// console.log(linha)
+// quantidadeAtual++
+// }
+
+
+
+//A - a) Se a quantidade for 0, imprima no console "Que pena! Você pode adotar um pet!"
+
+//B Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array.
+
+//Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array
+
+//Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável.
+
+//Exercícios de escrita de código.
+
+// 1
+
+//Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável. 
+
+// let novoBichinho = [ ]
+// let nomeBichinho = [ ]
+// let numeroBichinho = Number (prompt("Voce tem algum pet"))
+// if ( numeroBichinho === 0 ) {
+// console.log("Que pena! Voce pode adotar um pet")
+// } 
+// else {  
+
+// for (let i = 0; i < numeroBicho; i++){
+// let nome = prompt("Me fala o nome do seu bicho")
+// nomeBicho.push(nome)
+
+// }
+
+// }
+// console.log(numeroBicho)
+
+
+// // //b) Escreva um programa que imprime cada um dos valores do array original divididos por 10
+
+// // arrayOriginal = [23, 12, 10, 20, 10]
+
+
+// // function divididoPorDez(array){
+// //     let i = 0
+// //     while (i < 5){
+// //         resultado = array [1] / 10
+// //         console.log(resultado)
+// //         i ++
+
