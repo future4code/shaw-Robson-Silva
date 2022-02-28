4f8d4c1bd489ddecef5d28404cc20d75406681bb
Deodato33 <robsondeodato@yahoo.com.br>
2022-02-27 22:37:06 -0300

    projeto-black-jack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,41 +1,30 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
 
-    console.log("Bem-vindo(a) ao jogo de blackjack")
+console.log("Boas vindas ao jogo BlackJack")
+//Segundo passo: Crie um confirm pergutando ao usuário.
+const iniciarRodada = confirm("Quer iniciar uma nova rodada")
+// Terceiro passo: Se o usuário apertar cancel, finalizara o jogo.
+if (iniciarRodada) {
 
-let jogo = confirm("Quer iniciar uma nova rodada?")
+    let usuarioCartaUm = comprarCarta() // funcao para comprar carta que a Labenu forneceu.
+    let usuarioCartaDois = comprarCarta()
+    let somaUsuario = usuarioCartaUm.valor + usuarioCartaDois.valor
 
-if (jogo) {
-    let carta1Usuario = comprarCarta()
-    let carta2Usuario = comprarCarta()
-    let carta1Pc = comprarCarta()
-    let carta2Pc = comprarCarta()
+    let pcCartaUm = comprarCarta()
+    let pcCartaDois = comprarCarta()
+    let somaPc = pcCartaUm.valor + pcCartaDois.valor
 
-    let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
-    let pontuacaoPc = carta1Pc.valor + carta2Pc.valor
+    console.log(`Usuário - cartas: ${usuarioCartaUm.texto} ${usuarioCartaDois.texto}  - pontuação ${somaUsuario}`)
 
-    console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
-    console.log(`Computador - cartas: ${carta1Pc.texto} ${carta2Pc.texto} - ${pontuacaoPc}`)
-
-    if (pontuacaoUsuario > pontuacaoPc) {
-        console.log("O usuário ganhou!")
-    } else if (pontuacaoPc > pontuacaoUsuario) {
-        console.log("O computador ganhou!")
-    } else if (pontuacaoUsuario === pontuacaoPc) {
-        console.log("Empate!")
+    console.log(`computador - cartas: ${pcCartaUm.texto} ${pcCartaDois.texto}  - pontuação ${somaPc}`)
+if (somaUsuario > somaPc) {
+    console.log("O usuario ganhou!")
+} else if (somaPc > somaUsuario) {
+    console.log("O computador ganhou!")
+ } else {
+    console.log("Empate!")
     }
 
 } else {
     console.log("O jogo acabou")
-}
+} 
 
